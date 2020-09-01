f9906c999f28804f4dba1b67f90176cfbd2f81b7
Stefan Adams <stefan.adams@vipcomag.de>
2020-09-01 09:45:51 +0200

    no message on unknown port or socket - happens all the time in the real world!

--- stack/pico_socket.c
+++ stack/pico_socket.c
@@ -584,7 +584,7 @@ static int pico_socket_deliver(struct pico_protocol *p, struct pico_frame *f, ui
 
     sp = pico_get_sockport(p->proto_number, localport);
     if (!sp) {
-        dbg("No such port %d\n", short_be(localport));
+        //dbg("No such port %d\n", short_be(localport));
         return -1;
     }
 
@@ -1994,7 +1994,7 @@ int pico_transport_process_in(struct pico_protocol *self, struct pico_frame *f)
         return ret;
 
     if (!IS_BCAST(f)) {
-        dbg("Socket not found... \n");
+        //dbg("Socket not found... \n");
         pico_notify_socket_unreachable(f);
         ret = -1;
         pico_err = PICO_ERR_ENOENT;
